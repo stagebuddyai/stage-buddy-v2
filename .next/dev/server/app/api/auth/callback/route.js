454ecf96f16d32069297cd1736ac16a6fc8756ee
stagebuddyai <b.freedom.rocks@gmail.com>
2026-01-22 21:17:18 +0000

    Remove .next build artifacts from Git tracking

--- .next/dev/server/app/api/auth/callback/route.js
+++
@@ -1,8 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/callback/route.js")
-R.c("server/chunks/node_modules_next_6d9236d8._.js")
-R.c("server/chunks/node_modules_@supabase_auth-js_dist_module_02e1b12e._.js")
-R.c("server/chunks/node_modules_b59cd416._.js")
-R.c("server/chunks/[root-of-the-server]__d452691c._.js")
-R.c("server/chunks/_next-internal_server_app_api_auth_callback_route_actions_66c10d83.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/auth/callback/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/auth/callback/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/chunks/_next-internal_server_app_api_auth_callback_route_actions_66c10d83.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/auth/callback/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_auth_callback_route_actions_66c10d83.js.map
